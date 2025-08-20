9b1e120ea90f4a15ade0d1f2702a0d0bc6d5e16e
laiycedward <laiycedward@gmail.com>
2025-08-20 21:52:19 +0800

    Tutorial 2

--- Tutorial2Q1.py
+++ Tutorial2Q1.py
@@ -1,12 +1,4 @@
-#list to store leap years
-leap_years = []
-
-#test every year
 for i in range(1900, 2021):
-    #divisible by 4 by not 100
-    if i % 4 == 0 and i % 100 != 0:
-        leap_years.append(i)
-    #divisible by 400
-    elif i % 400 == 0:
-        leap_years.append(i)
-print(leap_years)
+    #divisible by 4 but not 100, or divisible by 400
+    if (i % 4 == 0 and i % 100 != 0) or (i % 400 == 0):
+        print(i)
