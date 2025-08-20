6f71907f3b69855d35fc4c511ca7a7c9342dbdbf
laiycedward <laiycedward@gmail.com>
2025-08-20 08:07:06 +0800

    added function

--- Tutorial1Q2.py
+++ Tutorial1Q2.py
@@ -9,18 +9,13 @@ c = 9
 #cos C = (a^2 + b^2 - c^2) / 2ab
 #math.acos function gives angle in radians
 #math.degrees function converts radians to degrees
-cosine_C = (a**2 + b**2 - c**2) / (2 * a * b)
-radian_C = math.acos(cosine_C)
-degree_C = math.degrees(radian_C)
-
-#repeat for angles B, A
-cosine_B = (a**2 + c**2 - b**2) / (2 * a * c)
-radian_B = math.acos(cosine_B)
-degree_B = math.degrees(radian_B)
-
-cosine_A = (b**2 + c**2 - a**2) / (2 * b * c)
-radian_A = math.acos(cosine_A)
-degree_A = math.degrees(radian_A)
+def find(a, b, c):
+    cosine_C = math.acos((a**2 + b**2 - c**2) / (2 * a * b))
+    return math.degrees(cosine_C)
+C = find(a, b, c)
+B = find(a, c, b)
+A = find(b, c, a)
 
 #print out the angles
-print("Angle A is: ", degree_A, ", Angle B is: ", degree_B, ", Angle C is: ", degree_C)
+print("Angle A is:", A, ", Angle B is:", B, ", Angle C is:", C)
+print("The 3 angles add up to", A + B + C)
